f86ea2528f032b0628cbbedb7bb25c90d0293d19
RihardsKluinis <rihardskluinis@gmail.com>
2024-05-21 00:48:29 +0300

    addedDatesForLaunces

--- seleniumScraperProductHunt.py
+++ seleniumScraperProductHunt.py
@@ -43,14 +43,14 @@ def closeConnectionScrapper(app):
 
 
 
-def getTheData(projectLink, founderLinks):    
+def getTheData(projectLink, founderLinks, dateOfPosting):    
     # Define the URL for running the actor synchronously
     url = "https://api.apify.com/v2/acts/lhotanova~product-hunt-profile-scraper/run-sync-get-dataset-items?token=apify_api_idXFjAfQnn8AQcPIN1cWCAowPc39EJ0do8YL"
 
     # Define the input data (profile URLs)
-    
     input_data = {
-    "profileUrls": founderLinks}
+        "profileUrls": founderLinks
+    }
 
     response = requests.post(url, json=input_data)
     # Make a POST request to run the actor synchronously with input data
@@ -69,9 +69,9 @@ def getTheData(projectLink, founderLinks):
         # Get the ID of the current profile
         try:
             profile_id = profile_data['id']
-        except:
-            print("Something Fucked Up in the DB process")
-            break
+        except KeyError:
+            print("Something went wrong in the DB process")
+            continue
 
         # Check if the project already exists in the dictionary
         if project_name in project_ids:
@@ -80,25 +80,22 @@ def getTheData(projectLink, founderLinks):
                 print(f"Profile {profile_id} already exists under project {project_name}, skipping...")
                 continue
         print("Project Name is", project_name)
+
         # Create a new child node under the main node for the project if it doesn't exist
-        if not main_ref.child(project_name).get():
-            project_ref = main_ref.child(project_name)
-        else:
-            project_ref = main_ref.child(project_name)
+        project_ref = main_ref.child(project_name)
 
         # Create a new child node under the project node for the profile
         profile_ref = project_ref.child(profile_id)
 
-        # Set the profile data
+        # Set the profile data along with the date
         profile_ref.set(profile_data)
+        project_ref.update({'date': dateOfPosting})
 
         # Add the profile ID to the set of used IDs for the project
         if project_name in project_ids:
             project_ids[project_name].add(profile_id)
         else:
             project_ids[project_name] = {profile_id}
-
-
 
     print("Profiles uploaded successfully to Firebase Realtime Database.")
 
@@ -153,9 +150,7 @@ for cookie in cookies:
 
 # Refresh the page to apply the cookies
 driver.refresh()
-time.sleep(3)
-driver.get('https://www.producthunt.com/leaderboard/monthly/2024/1')
-time.sleep(3)
+
 
 def is_visible(element):
     return element.is_displayed() and element.is_enabled()
@@ -177,89 +172,100 @@ def scrollDown():
     driver.execute_script(f"window.scrollBy(0, {scroll_amount});")
 
 # Loop to click on each item
-first = 0
+
+
+
+day = 1
 while True:
-    # Go trough first 1000 projects
-    for i in range(1, 1000):  # Assuming there are 84 elements on the page
-        print("We're at ", i)
-        if first == 0:
-            time.sleep(5)
-            first = 1
-        xpath = '//*[@id="__next"]/div/main/div/div[2]/div[' + str(i) + ']/div/div[1]/a[1]/div'
-       
-        attempt = 0
-        # Find the element of project
-        haveWeFoundAProduct = True
-        while attempt < 3:
-            try:
-                # Wait until the project element is clickable
-                WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, xpath)))
-                element = driver.find_element(By.XPATH, xpath)
-                # Click on the element
-                element.click()
-                break  # Break the loop if click succeeds
-
-            except :
-                # Increment the attempt counter
-                attempt += 1
-
-                if attempt < 3:
-                    print(f"Click intercepted. Retrying in 2 seconds (attempt {attempt}/{3})...")
-                    time.sleep(2)  # Wait for 5 seconds before retrying
-                else:
-                    haveWeFoundAProduct= False
-                    print("Maximum retry attempts reached for this element. Continuing with next element...")
-                    continue
-        if haveWeFoundAProduct == False:
-            continue
-        # Wait a little bit for the page to load
-        time.sleep(2)
-        potentialLinks = []
-        projectLink = ""
-        haveWeFoundSomeone = False
+    time.sleep(3)
+    driver.get('https://www.producthunt.com/leaderboard/daily/2024/5/'+day+'/all')
+    time.sleep(3)
+    day = day + 1
+    first = 1
+    for i in range(1, 100):  # Assuming there are 84 elements on the page
+        try:
+            print("We're at ", i)
+            if first == 1:
+                time.sleep(5)
+                first = 2
+            xpath = '//*[@id="__next"]/div/main/div/div[2]/div[' + str(i) + ']/div/div[1]/a[1]/div'
         
-        for j in range(3, 60):  #Checking up to 57 founders for each project
-            next_xpath = '//*[@id="about"]/div[3]/div[' + str(j) + ']'
+            attempt = 0
+            # Find the element of project
+            haveWeFoundAProduct = True
+            while attempt < 3:
+                try:
+                    # Wait until the project element is clickable
+                    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+                    element = driver.find_element(By.XPATH, xpath)
+                    # Click on the element
+                    element.click()
+                    break  # Break the loop if click succeeds
 
-            try:
-                print(1)
-                WebDriverWait(driver, 6).until(EC.element_to_be_clickable((By.XPATH, next_xpath)))
-                print(2)
-                next_element = driver.find_element(By.XPATH, next_xpath)
-                print(3)
+                except :
+                    # Increment the attempt counter
+                    attempt += 1
 
-                next_a_element = next_element.find_element(By.TAG_NAME, 'a')
-                nextFounderLink = next_a_element.get_attribute("href")
-                potentialLinks.append(nextFounderLink)
-                projectLink = driver.current_url
-                time.sleep(1)
-                print(4)
-                haveWeFoundSomeone = True
-                #Now were in the founders profile
-            except:
-                print(f"that's it for now")
-                backButton = driver.find_element(By.XPATH, '//*[@id="__next"]/div[1]/a')
-                backButton.click()
-                break
+                    if attempt < 3:
+                        print(f"Click intercepted. Retrying in 2 seconds (attempt {attempt}/{3})...")
+                        time.sleep(2)  # Wait for 5 seconds before retrying
+                    else:
+                        haveWeFoundAProduct= False
+                        print("Maximum retry attempts reached for this element. Continuing with next element...")
+                        continue
+            if haveWeFoundAProduct == False:
+                continue
+            # Wait a little bit for the page to load
+            time.sleep(2)
+            potentialLinks = []
+            projectLink = ""
+            haveWeFoundSomeone = False
             
-            time.sleep(1)
-        scrollDown()
+            for j in range(3, 60):  #Checking up to 57 founders for each project
+                next_xpath = '//*[@id="about"]/div[3]/div[' + str(j) + ']'
 
-            
+                try:
+                    print(1)
+                    WebDriverWait(driver, 6).until(EC.element_to_be_clickable((By.XPATH, next_xpath)))
+                    print(2)
+                    next_element = driver.find_element(By.XPATH, next_xpath)
+                    print(3)
+
+                    next_a_element = next_element.find_element(By.TAG_NAME, 'a')
+                    nextFounderLink = next_a_element.get_attribute("href")
+                    potentialLinks.append(nextFounderLink)
+                    projectLink = driver.current_url
+                    time.sleep(1)
+                    print(4)
+                    haveWeFoundSomeone = True
+                    #Now were in the founders profile
+                except:
+                    print(f"that's it for now")
+                    backButton = driver.find_element(By.XPATH, '//*[@id="__next"]/div[1]/a')
+                    backButton.click()
+                    break
                 
+                time.sleep(1)
+            scrollDown()
 
-        
-        founderLinks = []
-        # Wait for the page to load after navigating back
-        for link in potentialLinks:
-            if "@" in str(link):
-                print(link)
-                founderLinks.append(str(link))
-        if haveWeFoundSomeone == True and len(founderLinks)>0:
-            app = initScrapper()
-            getTheData(projectLink, founderLinks)
-            closeConnectionScrapper(app)
-        time.sleep(1)
+                
+                    
+
+            
+            founderLinks = []
+            # Wait for the page to load after navigating back
+            for link in potentialLinks:
+                if "@" in str(link):
+                    print(link)
+                    founderLinks.append(str(link))
+            dateOfPosting = str(day)+"/05/2024"
+            if haveWeFoundSomeone == True and len(founderLinks)>0:
+                app = initScrapper()
+                getTheData(projectLink, founderLinks)
+                closeConnectionScrapper(app)
+            time.sleep(1)
+        except:
+            print("We've seen everything today")
         
             
         
