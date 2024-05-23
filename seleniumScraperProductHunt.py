7703c1f0154ddffe5742b2d9ccef32a963c4d847
RihardsKluinis <rihardskluinis@gmail.com>
2024-05-23 19:27:56 +0300

    c

--- seleniumScraperProductHunt.py
+++ seleniumScraperProductHunt.py
@@ -111,13 +111,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--window-size=1920x1080")
-chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--disable-infobars")
-chrome_options.add_argument("--disable-notifications")
-chrome_options.add_argument("--disable-logging")
-chrome_options.add_argument("--log-level=3")
-chrome_options.add_argument("--single-process")
+
 
 
 chrome_path = "/app/.chrome-for-testing/chrome-linux64/chrome"
