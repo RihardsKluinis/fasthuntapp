96df0134d3dab5662ba7005b9436dc0cd5701ca7
RihardsKluinis <rihardskluinis@gmail.com>
2024-05-23 19:20:47 +0300

    c

--- seleniumScraperProductHunt.py
+++ seleniumScraperProductHunt.py
@@ -107,9 +107,17 @@ def getTheData(projectLink, founderLinks, dateOfPosting):
 
 # Set Chrome options
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # Optional: run in headless mode
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-sh-usage")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-infobars")
+chrome_options.add_argument("--disable-notifications")
+chrome_options.add_argument("--disable-logging")
+chrome_options.add_argument("--log-level=3")
+chrome_options.add_argument("--single-process")
 
 
 chrome_path = "/app/.chrome-for-testing/chrome-linux64/chrome"
