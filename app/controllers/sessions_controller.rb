061447a50ce13c9f3d287258f456fd6620b18ede
rihardskluinis <rihardskluinis@gmail.com>
2024-06-19 16:47:45 +0300

    changed sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,29 +1,22 @@
 # app/controllers/sessions_controller.rb
-class SessionsController < ApplicationController
-    def create
-      user = User.from_omniauth(request.env['omniauth.auth'])
-      session[:user_id] = user.id
-      redirect_to root_path, notice: 'Signed in!'
-    end
-  
-    def destroy
-        super do
-          # Turbo requires redirects be :see_other (303); so override Devise default (302)
-          return redirect_to after_sign_out_path_for(resource_name), status: :see_other
-        end
-      end
-    def update
-        if current_user.update(user_params)
-          redirect_to edit_settings_path, notice: 'Settings were successfully updated.'
-        else
-          render :edit
-        end
-    end
-    def user_params
-        params.require(:user).permit(:theme, :notification_preferences)
-    end
-    def failure
-      redirect_to root_path, alert: 'Authentication failed, please try again.'
+class UserSessionsController < ApplicationController
+  protect_from_forgery with: :null_session # Adjust CSRF protection as needed
+
+  def create
+    session_data = user_session_params
+
+    @user_session = UserSession.new(session_data)
+
+    if @user_session.save
+      render json: { message: 'User session created successfully' }, status: :created
+    else
+      render json: { errors: @user_session.errors.full_messages }, status: :unprocessable_entity
     end
   end
-  
+
+  private
+
+  def user_session_params
+    params.require(:user_session).permit(:user_id, :profile_id, :linkedin, :linkedin_password, :linkedin_email)
+  end
+end
