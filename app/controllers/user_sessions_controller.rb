d9d907e26795b07b846c7844b6d1af0bd28e109a
rihardskluinis <rihardskluinis@gmail.com>
2024-06-19 18:49:19 +0300

    changed the way we look for linkedin Link

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -16,7 +16,7 @@ class UserSessionsController < ApplicationController
     private
   
     def user_session_params
-      params.require(:user_session).permit(:type, :profile_id, :checked, :action, :user_id, :linkedin_password, :linkedin_username, :profile_linkedin)
+      params.require(:user_session).permit(:profile_id, :user_id, :linkedin_password, :linkedin_username, :profile_linkedin)
     end
   end
   
