a7c2a04e81af071d773dda17d6bbfa2505a9571b
rihardskluinis <rihardskluinis@gmail.com>
2024-06-22 16:27:18 +0300

    Added functionality that checks for incorrect linkedin pasword in past attempts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   
     before_action :authenticate_user!
 
-    
+
     before_action :check_incorrect_password_attempts, if: :user_signed_in?
     private
 
@@ -11,5 +11,10 @@ class ApplicationController < ActionController::Base
       if current_user.user_sessions.exists?(is_this_password_correct: false)
         flash.now[:alert] = "There is an incorrect password attempt in your history."
       end
+    end
+    def update_user_sessions
+      user_sessions = current_user.user_sessions.where(is_this_password_correct: false)
+      user_sessions.update_all(username: params[:username], password: params[:password], is_this_password_correct: true)
+      redirect_to root_path, notice: 'User sessions updated successfully.'
     end
   end
