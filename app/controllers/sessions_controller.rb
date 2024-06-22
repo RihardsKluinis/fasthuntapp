0d498c24714c80dbac331a475960096ac688ffb0
rihardskluinis <rihardskluinis@gmail.com>
2024-06-22 16:07:28 +0300

    Added functionality that checks for incorrect linkedin pasword in past attempts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,34 +1,35 @@
-# app/controllers/sessions_controller.rb
 class SessionsController < ApplicationController
-    def create
-      user = User.from_omniauth(request.env['omniauth.auth'])
-      session[:user_id] = user.id
+  def create
+    user = User.from_omniauth(request.env['omniauth.auth'])
+    session[:user_id] = user.id
+    
+    if user.user_sessions.exists?(is_this_password_correct: false)
+      flash[:alert] = "There is an incorrect password attempt in your history."
+    end
 
-      if user.user_sessions.exists?(is_this_password_correct: false)
-        flash[:alert] = "There is an incorrect password attempt in your history."
-      end
+    redirect_to root_path, notice: 'Signed in!'
+  end
 
-      redirect_to root_path, notice: 'Signed in!'
-    end
-  
-    def destroy
-        super do
-          # Turbo requires redirects be :see_other (303); so override Devise default (302)
-          return redirect_to after_sign_out_path_for(resource_name), status: :see_other
-        end
-      end
-    def update
-        if current_user.update(user_params)
-          redirect_to edit_settings_path, notice: 'Settings were successfully updated.'
-        else
-          render :edit
-        end
-    end
-    def user_params
-        params.require(:user).permit(:theme, :notification_preferences)
-    end
-    def failure
-      redirect_to root_path, alert: 'Authentication failed, please try again.'
+  def destroy
+    # Adjusted for Devise if needed, otherwise remove `super`
+    super do
+      return redirect_to after_sign_out_path_for(resource_name), status: :see_other
     end
   end
-  
+
+  def update
+    if current_user.update(user_params)
+      redirect_to edit_settings_path, notice: 'Settings were successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:theme, :notification_preferences)
+  end
+
+  def failure
+    redirect_to root_path, alert: 'Authentication failed, please try again.'
+  end
+end
