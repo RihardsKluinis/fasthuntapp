716a7122d078bb6407a888d652910192c531fead
rihardskluinis <rihardskluinis@gmail.com>
2024-06-25 17:00:27 +0300

    Added functionality that checks for incorrect linkedin pasword in past attempts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   root "projects#index"
   resources :projects, only: [:index, :show, :edit, :update, :destroy]
+  resources :checkmarks, only: [:create, :update]
+  resources :project_checkmarks, only: [:create, :update]
+  resources :profiles, only: [:show, :edit, :update, :destroy]
+  resources :user_sessions, only: [:create]
+
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
+  match '/auth/:provider/callback', to: 'users/omniauth_callbacks#google_oauth2', via: [:get, :post]
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
 
   post 'update_user_session', to: 'application#update_user_session'
   # Define any other routes here
